3fa090427a5ee864d8fbb623a0b69c74ba565dd8
Gauravbhatt19 <gauravbhatt1924@gmail.com>
2021-02-04 01:18:35 +0530

    Add Popup page structure

--- popup/index.js
+++ popup/index.js
@@ -1,3 +1,15 @@
 /**
  * Popup page js support here
  */
+$(function() {
+	$('.notify-all').change(function() {
+		$('.toggle-switch input[type="checkbox"]').prop('checked', $(this).is(':checked'));
+	});
+	$('input[type="checkbox"]').not('.notify-all').change(function() {
+		var isAnyChecked = true;
+		$.each($('input[type="checkbox"]').not('.notify-all'), function() {
+			isAnyChecked = isAnyChecked && $(this).is(':checked');
+		});
+		$('.notify-all').prop('checked', isAnyChecked);
+	});
+});
