4981f5690c5041b914d3ef89fbee29ab2ace275d
Gauravbhatt19 <gauravbhatt1924@gmail.com>
2021-02-04 23:38:06 +0530

    Checked notifications state saving

--- popup/index.js
+++ popup/index.js
@@ -4,22 +4,39 @@ $(function() {
 	});
 	$('.notifications-container').on('change', 'input[type="checkbox"]', function() {
 		if (!$(this).hasClass('notify-all')) {
-			var isAnyChecked = true;
+			var isAllChecked = true;
 			$.each($('input[type="checkbox"]').not('.notify-all'), function() {
-				isAnyChecked = isAnyChecked && $(this).is(':checked');
+				isAllChecked = isAllChecked && $(this).is(':checked');
 			});
-			$('.notify-all').prop('checked', isAnyChecked);
+			$('.notify-all').prop('checked', isAllChecked);
 		}
+		chrome.storage.sync.set({'checkedNotifications': getCheckedNotificationIDs()});
 	});
 
 	var toggleSwitchElements = '<div><span class="on">On</span><span class="off">Off</span></div><i></i>';
 
-	notifications.forEach(function(value){
-		var toggleSwitch = '<span class="toggle-switch float-right"><input type="checkbox" class="d-none '+value.id+'">'+toggleSwitchElements+'</span>';
-		$('<div/>', {
-			html: '<label class="fz-18 c-pointer user-select-none">'+value.title+toggleSwitch+'</label>',
-			class: "py-3"
-		}).appendTo('.notifications-container');
-		$('<hr class="w-275 border-1 border-gray">').appendTo('.notifications-container');
+	chrome.storage.sync.get(['checkedNotifications'], function(storedData) {
+		var isAllChecked = true;
+		notifications.forEach(function(value){
+			var isChecked = storedData.checkedNotifications.includes(value.id);
+			isAllChecked = isAllChecked && isChecked;
+			var toggleSwitch = '<span class="toggle-switch float-right"><input type="checkbox" '+(isChecked?'checked="checked"':'')+'class="d-none" id="'+value.id+'">'+toggleSwitchElements+'</span>';
+			$('<div/>', {
+				html: '<label class="fz-18 c-pointer user-select-none">'+value.title+toggleSwitch+'</label>',
+				class: "py-3"
+			}).appendTo('.notifications-container');
+			$('<hr class="w-275 border-1 border-gray">').appendTo('.notifications-container');
+		});
+		$('.notify-all').prop('checked', isAllChecked);
 	});
 });
+
+function getCheckedNotificationIDs() {
+	var checkedNotificationIDs = new Array();
+	$.each($('input[type="checkbox"]').not('.notify-all'), function() {
+		if( $(this).is(':checked') ) {
+			checkedNotificationIDs.push( $(this).attr('id') );
+		}
+	});
+	return checkedNotificationIDs;
+}
